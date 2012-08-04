96f790bee6a687b1341bdb9d36f743a583d125cf
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-04 17:27:10 +0100

    Add templates and stylesheet to main project.

--- yrsbadger/settings.py
+++ yrsbadger/settings.py
@@ -111,9 +111,7 @@ ROOT_URLCONF = 'yrsbadger.urls'
 WSGI_APPLICATION = 'yrsbadger.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+  os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates')),
 )
 
 INSTALLED_APPS = (
