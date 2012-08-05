580c1a753b318c3c4e4b59c7fe03e2a4a07e8778
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-05 22:49:30 +0100

    Change loadsubmissions to only send one email.

--- loader/management/commands/loadsubmissions.py
+++ loader/management/commands/loadsubmissions.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from minibadge.models import Award, Badge
 from google_spreadsheet.api import SpreadsheetAPI
 import os
+import minibadge.mailer
 
 class Command(BaseCommand):
   args = '<one>'
@@ -13,14 +14,17 @@ class Command(BaseCommand):
     worksheet = client.get_worksheet("0AuaHiEjutiMUdE5SWTNvTUJTdlJ1ZXV3dUxoQkh6SGc", 1)
 
     for row in worksheet.get_rows():
+      awarded = False
       for title in map(str.strip, row["badgestoaward"].split(",")):
         try:
           badge = Badge.objects.get(title=title)
           if not row["youngpersonsemailaddress"]:
             print("ERROR: No email address")
           elif not Award.objects.filter(badge=badge, email=row["youngpersonsemailaddress"]):
+            awarded = True
             award = Award.objects.create(badge=badge, email=row["youngpersonsemailaddress"])
             award.save()
-            award.send()
         except Badge.DoesNotExist:
           print("ERROR: Badge does not exist [%s] for %s" % (title, row["youngpersonsemailaddress"]))
+      if awarded:
+        minibadge.mailer.send(row["youngpersonsemailaddress"])
